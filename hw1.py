184db3b8f47d95a824c895b474fc8b14102f32fc
jtoulzac <jean.toulzac@ens-rennes.fr>
2025-09-09 11:17:29 +0200

    calcul de F sur genome_hw1.fa

--- hw1.py
+++ hw1.py
@@ -1,4 +1,22 @@
-def F(k, w):
+def parser(filename):
+    f = open(filename, "r")
+    dico = []
+    w_buffer = ""
+
+    for line in f :
+        if line[0] == '>':
+            dico.append(w_buffer)
+            w_buffer = ""
+        else:
+            w_buffer = ''.join((w_buffer, line[0:(len(line))-1]))
+    dico.append(w_buffer)
+
+    f.close()
+    return dico
+
+sequences = parser("genome_hw1.fa")
+
+def F_one_word(k, w):
     '''
     F(k) is the number of distinct subwords of length k in w
     '''
@@ -8,7 +26,22 @@ def F(k, w):
         sw = w[i:i+k]
         compteur += not(sw in dict)
         dict[sw] = 1
+    return (compteur,dict)
+
+def F_many_words(k, words):
+    compteur, dict = F_one_word(k, words[0])
+    for w in words:
+        for i in range(len(w) - k + 1):
+            sw = w[i:i+k]
+            compteur += not(sw in dict)
+            dict[sw] = 1
     return compteur
+
+
+F_sequences = []
+for k in range(2,31):
+    F_sequences.append(F_many_words(k,sequences))
+print(F_sequences)
 
 def Fibonacci_w (l):
     if (l < 1):
@@ -29,9 +62,3 @@ def Fibonacci_w (l):
             n0 = n - n0
         return word
 
-print(Fibonacci_w(1))
-print(Fibonacci_w(2))
-print(Fibonacci_w(3))
-print(Fibonacci_w(5))
-print(Fibonacci_w(8))
-print(Fibonacci_w(30))
