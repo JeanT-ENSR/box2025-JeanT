e03f5e57560198a32e914a1d2eb41dd490a78109
jtoulzac <jean.toulzac@ens-rennes.fr>
2025-11-12 11:07:20 +0100

    implementation du bloom filter de base fini

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -1,9 +1,36 @@
 import numpy as np
-import mmh3 # librairie de fonctions de hash
+# import mmh3 # librairie de fonctions de hash
 
 nb_hashes = 7
 size_max = 100000000
+
 item = "ACGGACGACGACT"
-for seed in range(nb_hashes):
-    key = mmh3.hash(item, seed, signed=false) % size_max
-    print(f"Seed {seed} is {key}")
+#for seed in range(nb_hashes):
+#    key = mmh3.hash(item, seed, signed=false) % size_max
+#    print(f"Seed {seed} is {key}")
+
+def hash(item, seed):
+    sum = 0
+    for i in range(len(item)):
+        v = ord(item[i])
+        sum = (sum + v)*seed
+    return sum
+
+bloom_array = np.zeros(size_max, dtype=bool)
+
+def insert(item):
+    for seed in range(nb_hashes):
+        key = hash(item,seed) % size_max
+        bloom_array[key] = True
+    return
+
+def query(item):
+    is_in = True
+    for seed in range(nb_hashes):
+        key = hash(item,seed) % size_max
+        is_in = is_in and bloom_array[key]
+    return is_in
+
+insert(item)
+print(query(item))
+print(query("ACAC"))
