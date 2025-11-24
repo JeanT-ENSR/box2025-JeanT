2715d459e541465f29ad912e6b00a3f952d34bb5
jtoulzac <jean.toulzac@ens-rennes.fr>
2025-11-24 17:12:28 +0100

    J'ai reussi à faire fonctionner mmh3

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -1,33 +1,28 @@
+#!/usr/bin/env python
+
 import numpy as np
-# import mmh3 # librairie de fonctions de hash
+import mmh3 # librairie de fonctions de hash
 
 nb_hashes = 7
 size_max = 100000000
 
 item = "ACGGACGACGACT"
-#for seed in range(nb_hashes):
-#    key = mmh3.hash(item, seed, signed=false) % size_max
-#    print(f"Seed {seed} is {key}")
-
-def hash(item, seed):
-    sum = 0
-    for i in range(len(item)):
-        v = ord(item[i])
-        sum = (sum + v)*seed
-    return sum
+for seed in range(nb_hashes):
+    key = mmh3.hash(item, seed, signed=False) % size_max
+    print(f"Seed {seed} is {key}")
 
 bloom_array = np.zeros(size_max, dtype=bool)
 
 def insert(item):
     for seed in range(nb_hashes):
-        key = hash(item,seed) % size_max
+        key = mmh3.hash(item,seed, signed=False) % size_max
         bloom_array[key] = True
     return
 
 def query(item):
     is_in = True
     for seed in range(nb_hashes):
-        key = hash(item,seed) % size_max
+        key = mmh3.hash(item,seed, signed=False) % size_max
         is_in = is_in and bloom_array[key]
     return is_in
 
