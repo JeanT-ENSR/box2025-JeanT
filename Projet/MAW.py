7e4d290242ccca76f9ff664a0825c0884b75690e
jtoulzac <jean.toulzac@ens-rennes.fr>
2025-11-26 11:16:32 +0100

    ajout des fichiers

--- Projet/MAW.py
+++ Projet/MAW.py
@@ -3,8 +3,27 @@
 from xopen import xopen
 from readfa import readfq
 import numpy as np
-import sys
-from collections import defaultdict
+# from collections import defaultdict
+
+##### Utility function #####
+
+def change(c):
+    if c == 'A':
+        return 'T'
+    if c == 'T':
+        return 'A'
+    if c == 'C':
+        return 'G'
+    if c == 'G':
+        return 'C'
+    return c
+
+def reverseComplement(read):
+    s = list(read)
+    l = len(s)
+    for i in range(l):
+        s[i] = change(s[i])
+    return ''.join(list(reversed(s))) 
 
 def open(fastaFilePath):
     sequences = []
@@ -12,4 +31,41 @@ def open(fastaFilePath):
         for _,seq,_ in readfq(fa):
             sequences.append(seqfa)
     return sequences
+
+
+##### MAW function #####
+
+def isXInSeq(x,seq):
+    '''
+    Return True iff x or its reverse complement is in seq
+    '''
+    isIn = False
+    k = len(x)
+    l = len(seq)
+    reverseX = reverseComplement(x)
+    for i in range(l-k):
+        subSeq = seq[i:i+k]
+        isIn = isIn or (subSeq == x) or (subSeq == reverseX)
+    return isIn
+
+# Test passed
+assert(isXInSeq("AC","TTACTT"))
+assert(isXInSeq("AC","GTTG"))
+assert(not isXInSeq("AC","TTTTTT"))
+assert(not isXInSeq("AC","GATCG"))
+
+def subSeq(sequence,kmin,kmax):
+    l = len(sequence)
+    subSequence = {}
+    for k in range(kmin,kmax):
+        for i in range(l-k):
+            subSequence[sequence[i:i+k]] = True
+    return subSequence
+
+def subSeqSequences(sequences,kmin,kmax):
+    subSeqSequences = defaultdict(list) # Need to store the name of the sequences
+    for sequence in sequences:
+        subSequence = subSeq(sequence,kmin,kmax)
+        for sub in subSequence:
+            subSeqSequences[sub].append(sub)
 
---
+++ Projet/Software_Report.md
@@ -0,0 +1,7 @@
+# Software Report
+
+## Program 1 : Minimum Absent Word (MAW)
+    Caculate all the subsequences between kmin and kmax
+
+
+## Program 2 : Minimum p-Absent Word (pMAW)
